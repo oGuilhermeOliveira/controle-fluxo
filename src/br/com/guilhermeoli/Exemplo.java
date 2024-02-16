ba9c7ccc3a9237fccc05af6c6dbf6d26de39dbdf
Guilherme Oliveira dos Santos <guilhermeolsnto@gmail.com>
2024-02-16 19:08:47 -0300

    first commit

---
+++ src/br/com/guilhermeoli/Exemplo.java
@@ -0,0 +1,33 @@
+package br.com.guilhermeoli;
+
+import java.util.Scanner;
+
+/**
+ * @author guiol
+ */
+public class Exemplo {
+
+    public static void main(String[] args) {
+        //     int result = 4;
+
+        //     if (result > 1) {
+        //         System.out.println("Resultado maior que um");
+        //     } else {
+        //         System.out.println("Resultado menor que um");
+        //     }
+        // }
+        //        Scanner s = new Scanner(System.in);
+        //
+        //        System.out.println("Digite sua idade: ");
+        //        int idade = s.nextInt();
+        //        if (idade < 18) {
+        //            System.out.println("Você é de menor");
+        //        } else {
+        //            System.out.println("Você é de maior");
+        //        }
+        //
+        //
+        //        System.out.println("Você tem " + idade + " anos");
+    }
+
+}
---
+++ src/br/com/guilhermeoli/TabuadaFor.java
@@ -0,0 +1,19 @@
+package br.com.guilhermeoli;
+
+import java.util.Scanner;
+
+/**
+ * @author guiol
+ */
+public class TabuadaFor {
+
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+
+        System.out.println("Digite um número para gerar a tabuada");
+        int num = s.nextInt();
+        for (int i = 0; i <= 10; i++) {
+            System.out.println(num + "X" + i + "=" + num*i);
+        }
+    }
+}
